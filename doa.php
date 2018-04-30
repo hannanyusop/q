4deb916fe24c8ace60be45105c61363717bb11ee
Hannan Yusop <hannanyusop@lynk.my>
2018-05-01 01:24:35 +0800

    add hits for doa

--- doa.php
+++ doa.php
@@ -17,6 +17,8 @@
         if ($fetch_surah) {
 
             $data = mysqli_fetch_assoc($fetch_surah);
+            $sql = "UPDATE doa SET hits = hits + 1 WHERE id = $did";
+            mysqli_query($db, $sql);
         }else{
 
             echo '<script>alert("Doa tidak wujud!");window.location="utama-doa.php"</script>';
@@ -36,14 +38,6 @@
             </nav>
             <div class="row">
                 <div class="col-xl-4">
-                    <!-- Subscribe -->
-                    <div class="block block-rounded">
-                        <div class="block-content">
-                            <a class="btn btn-block btn-hero btn-noborder btn-rounded btn-success mb-10" href="javascript:void(0)">Subscribe from $19/month</a>
-                            <p class="text-center">or <a class="link-effect" href="javascript:void(0)">buy this course for $14</a></p>
-                        </div>
-                    </div>
-                    <!-- END Subscribe -->
 
                     <!-- Course Info -->
                     <div class="block block-rounded">
@@ -54,9 +48,6 @@
                             </h3>
                         </div>
                         <div class="block-content">
-                            <div class="font-size-sm text-muted text-center mb-20">
-                                (<?php echo  'muka surat';?>)
-                            </div>
                             <table class="table table-borderless table-striped">
                                 <tbody>
                                  <?php $q_suggest = mysqli_query($db, "SELECT * FROM doa WHERE id != $did ORDER BY hits DESC LIMIT 10 ");
@@ -80,16 +71,16 @@
                     <div class="block block-rounded">
                         <div class="block-content">
                             <nav class="clearfix push">
-                                <button type="button" class="btn btn-square btn-secondary mr-5 mb-5">
-                                    <i class="fa fa-play mr-5"></i>Play
-                                </button>
-                                <button type="button" class="btn btn-square btn-secondary mr-5 mb-5">
-                                    <i class="fa fa-download mr-5"></i>Download
-                                </button>
                                 <h4 class="display-4">Doa <?php echo $data['title'] ?></h4><hr>
                                 <p class="p-12 h5"><?php echo $data['arab_text'] ?></p><br>
                                 <p class="text-muted h6"><mark><?php echo $data['malay_text'] ?></mark></p><br>
                                 <p class="p-10 bg-primary-lighter text-primary-dark h6"><b>Maksudnya:</b><br>"<i><?php echo $data['means'] ?></i>" </p>
+                                <?php if($data['track'] != ""): ?>
+                                    <audio controls>
+                                        <source src='<?php echo $data['track'] ?>' type='audio/mp3'>
+                                        Your browser does not support the audio tag.
+                                    </audio>
+                                <?php else: echo '<small class="p-10 bg-primary-lighter text-primary-dark font-w500">tiada track bagi surah ini</small>'; endif;?>
                             </nav>
                         </div>
                     </div>
